2b94398cd70dfae38f616fc04f588eba88577659
Jose Rocha <jsantosrocha@gmail.com>
2024-05-04 20:38:12 -0300

    modified docker-compose, build.gradle and application.yml for jwt and spring security

--- build.gradle.kts
+++ build.gradle.kts
@@ -68,6 +68,11 @@ dependencies {
 
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.+")
 
+    implementation("io.jsonwebtoken:jjwt-api:0.12.5")
+    implementation("io.jsonwebtoken:jjwt-impl:0.12.5")
+    implementation("io.jsonwebtoken:jjwt-jackson:0.12.5")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+
     // Test
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
@@ -78,6 +83,7 @@ dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("org.awaitility:awaitility-kotlin:4.+")
+    testImplementation("org.springframework.security:spring-security-test")
 
     // Liquibase
     liquibaseRuntime("info.picocli:picocli:4.+")

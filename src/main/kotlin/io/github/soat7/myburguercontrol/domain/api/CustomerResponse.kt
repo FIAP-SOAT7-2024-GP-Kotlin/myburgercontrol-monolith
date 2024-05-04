cd3edf42274bda0bb41790e9b66d00dd3c991a6d
Luiz Fernando Calazans <luiz.calazans@ifood.com.br>
2024-05-04 20:50:45 -0300

    update response variable type

--- src/main/kotlin/io/github/soat7/myburguercontrol/domain/api/CustomerResponse.kt
+++ src/main/kotlin/io/github/soat7/myburguercontrol/domain/api/CustomerResponse.kt
@@ -3,7 +3,7 @@ package io.github.soat7.myburguercontrol.domain.api
 import java.util.UUID
 
 data class CustomerResponse(
-    val id: UUID?,
+    val id: UUID,
     val cpf: String,
     val name: String,
     val email: String

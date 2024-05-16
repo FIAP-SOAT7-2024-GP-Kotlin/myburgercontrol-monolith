36b6711562a3ed8d18e62cb63a7a0983ef4044dd
FlorencioFer <fur_oliveira@hotmail.com>
2024-05-15 21:46:09 -0300

    fixing review

--- src/main/kotlin/io/github/soat7/myburguercontrol/domain/service/PaymentService.kt
+++ src/main/kotlin/io/github/soat7/myburguercontrol/domain/service/PaymentService.kt
@@ -1,15 +1,16 @@
 package io.github.soat7.myburguercontrol.domain.service
 
-import io.github.soat7.myburguercontrol.application.ports.inbound.PaymentServiceUseCase
+import io.github.soat7.myburguercontrol.application.ports.inbound.PaymentServicePort
 import io.github.soat7.myburguercontrol.application.ports.outbound.PaymentIntegrationPort
 import io.github.soat7.myburguercontrol.domain.model.Payment
 import mu.KLogging
 
 class PaymentService(
     private val paymentIntegrationPort: PaymentIntegrationPort
-) : PaymentServiceUseCase {
+) : PaymentServicePort {
 
     companion object : KLogging()
+
     override fun requestPayment(payment: Payment): Payment {
         logger.info { "Starting to request payment integration with id: [${payment.id}]" }
 

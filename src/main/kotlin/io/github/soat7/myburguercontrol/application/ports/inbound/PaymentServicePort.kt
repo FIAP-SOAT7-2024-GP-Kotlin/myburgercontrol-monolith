f08cc75abc588348a16679d47bd85ef53e883898
FlorencioFer <fur_oliveira@hotmail.com>
2024-05-15 22:18:53 -0300

    fixing review

--- src/main/kotlin/io/github/soat7/myburguercontrol/application/ports/inbound/PaymentServicePort.kt
+++ src/main/kotlin/io/github/soat7/myburguercontrol/application/ports/inbound/PaymentServicePort.kt
@@ -2,7 +2,7 @@ package io.github.soat7.myburguercontrol.application.ports.inbound
 
 import io.github.soat7.myburguercontrol.domain.model.Payment
 
-interface PaymentServiceUseCase {
+interface PaymentServicePort {
 
     fun requestPayment(payment: Payment): Payment
 }

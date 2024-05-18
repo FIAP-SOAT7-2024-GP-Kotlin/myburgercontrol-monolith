f6372fbe7a5585a5e2fbc5f4f7e9c5e421a57afd
Luiz Fernando Calazans <luiz.calazans@ifood.com.br>
2024-05-18 13:32:36 -0300

    Add ProblemDetail to GlobalExceptionHandler

--- src/main/kotlin/io/github/soat7/myburguercontrol/infrastructure/common/GlobalExceptionHandler.kt
+++ src/main/kotlin/io/github/soat7/myburguercontrol/infrastructure/common/GlobalExceptionHandler.kt
@@ -1,19 +1,20 @@
 package io.github.soat7.myburguercontrol.infrastructure.common
 
-import com.fasterxml.jackson.core.JsonProcessingException
 import io.github.soat7.myburguercontrol.domain.exception.ReasonCode
 import io.github.soat7.myburguercontrol.domain.exception.ReasonCodeException
-import io.github.soat7.myburguercontrol.infrastructure.rest.common.ErrorResponse
 import mu.KLogging
 import mu.withLoggingContext
 import org.springframework.core.annotation.Order
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
+import org.springframework.http.ProblemDetail
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.context.request.WebRequest
 import org.springframework.web.server.ResponseStatusException
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
+import java.net.URI
 
 @ControllerAdvice
 @Order(-2)
@@ -24,35 +25,38 @@ class GlobalExceptionHandler : ResponseEntityExceptionHandler() {
     internal data class HttpError(
         val httpStatus: HttpStatus,
         val code: String,
-        val messages: List<String>
+        val message: String
     ) {
-        constructor(reasonCode: ReasonCode) : this(reasonCode.status, reasonCode.code, listOf(reasonCode.description))
+        constructor(reasonCode: ReasonCode) : this(reasonCode.status, reasonCode.code, reasonCode.description)
     }
 
     @ExceptionHandler(Throwable::class)
-    fun handleException(t: Throwable): ResponseEntity<Any> {
+    fun handleException(t: Throwable, request: WebRequest): ResponseEntity<ProblemDetail> {
         val error = when (t) {
             is ReasonCodeException -> HttpError(t.reasonCode)
             is ResponseStatusException -> HttpError(
                 HttpStatus.valueOf(t.statusCode.value()),
                 ReasonCode.UNEXPECTED_ERROR.code,
-                listOf(t.reason ?: "Unexpected error")
+                t.reason ?: "An unexpected error occurred"
             )
 
             else -> HttpError(ReasonCode.UNEXPECTED_ERROR)
         }
         log(t, error)
 
-        val body: Any = try {
-            ErrorResponse(error.code, error.messages)
-        } catch (e: JsonProcessingException) {
-            ""
+        val problemDetail = ProblemDetail.forStatusAndDetail(
+            error.httpStatus,
+            error.message
+        ).apply {
+            title = error.httpStatus.reasonPhrase
+            detail = t.message ?: "Unexpected  error occurred"
+            instance = URI.create(request.getDescription(false))
         }
 
         return ResponseEntity
             .status(error.httpStatus)
             .contentType(MediaType.APPLICATION_JSON)
-            .body(body)
+            .body(problemDetail)
     }
 
     private fun log(t: Throwable, error: HttpError) {
--- src/main/kotlin/io/github/soat7/myburguercontrol/infrastructure/rest/common/ErrorResponse.kt
+++
@@ -1,6 +0,0 @@
-package io.github.soat7.myburguercontrol.infrastructure.rest.common
-
-data class ErrorResponse(
-    val code: String,
-    val messages: List<String>
-)

84ffbef65f08ecba6e9834b1f4aeb9321b38b895
Luiz Fernando Calazans <luiz.calazans@ifood.com.br>
2024-05-23 19:59:34 -0300

    Add authentication structure for IT

--- src/main/kotlin/io/github/soat7/myburguercontrol/infrastructure/rest/AuthController.kt
+++ src/main/kotlin/io/github/soat7/myburguercontrol/infrastructure/rest/AuthController.kt
@@ -3,16 +3,22 @@ package io.github.soat7.myburguercontrol.infrastructure.rest
 import io.github.soat7.myburguercontrol.application.ports.inbound.AuthenticationServicePort
 import io.github.soat7.myburguercontrol.infrastructure.rest.api.AuthRequest
 import io.github.soat7.myburguercontrol.infrastructure.rest.api.AuthResponse
+import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping(
+    path = ["/auth"],
+    produces = [MediaType.APPLICATION_JSON_VALUE],
+    consumes = [MediaType.APPLICATION_JSON_VALUE]
+)
 class AuthController(
     private val authenticationService: AuthenticationServicePort
 ) {
+
     @PostMapping
     fun authenticate(@RequestBody authRequest: AuthRequest): AuthResponse =
         authenticationService.authenticate(authRequest)
--- src/test/kotlin/io/github/soat7/myburguercontrol/base/BaseIntegrationTest.kt
+++ src/test/kotlin/io/github/soat7/myburguercontrol/base/BaseIntegrationTest.kt
@@ -2,11 +2,24 @@ package io.github.soat7.myburguercontrol.base
 
 import io.github.soat7.myburguercontrol.Application
 import io.github.soat7.myburguercontrol.container.PostgresContainer
+import io.github.soat7.myburguercontrol.domain.model.Role
+import io.github.soat7.myburguercontrol.fixtures.AuthFixtures
+import io.github.soat7.myburguercontrol.fixtures.UserFixtures
+import io.github.soat7.myburguercontrol.infrastructure.persistence.user.repository.UserRepository
+import io.github.soat7.myburguercontrol.infrastructure.rest.api.AuthResponse
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.test.web.client.postForEntity
+import org.springframework.boot.test.web.server.LocalServerPort
+import org.springframework.http.MediaType
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.test.context.ActiveProfiles
+import org.springframework.util.LinkedMultiValueMap
+import org.springframework.util.MultiValueMap
+import java.util.UUID
 
 @ActiveProfiles("test")
 @SpringBootTest(
@@ -18,4 +31,47 @@ class BaseIntegrationTest {
 
     @Autowired
     protected lateinit var restTemplate: TestRestTemplate
+
+    @Autowired
+    protected lateinit var userRepository: UserRepository
+
+    @Autowired
+    protected lateinit var passwordEncoder: PasswordEncoder
+
+    @LocalServerPort
+    private var port: Int = 0
+
+    protected lateinit var authenticationHeader: MultiValueMap<String, String>
+
+    @BeforeEach
+    fun setUpAuthentication() {
+        println("Cleaning User database...")
+        userRepository.deleteAll()
+        authenticationHeader = buildAuthentication()
+    }
+
+    protected fun buildAuthentication(): MultiValueMap<String, String> {
+        val cpf = "15666127055"
+        val password = UUID.randomUUID().toString()
+        val userRole = Role.ADMIN
+        userRepository.save(
+            UserFixtures.mockUserEntity(
+                cpf = cpf,
+                password = passwordEncoder.encode(password),
+                role = userRole
+            )
+        )
+
+        val response = restTemplate.postForEntity<AuthResponse>(
+            "/auth",
+            AuthFixtures.mockAuthCreationRequest(cpf, password)
+        ).body
+            ?: throw RuntimeException("Failed to authenticate")
+
+        val header: MultiValueMap<String, String> = LinkedMultiValueMap()
+        header.add("Authorization", "Bearer ${response.accessToken}")
+        header.add("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+
+        return header
+    }
 }

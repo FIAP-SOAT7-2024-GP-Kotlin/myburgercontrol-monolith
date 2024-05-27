9a4d3c3e243f3ea67a92c7accdf8fae406e34876
Andre Santos <alssantos.482@gmail.com>
2024-05-27 01:44:29 -0300

    feature: Added journeys swagger.

--- src/main/kotlin/io/github/soat7/myburguercontrol/infrastructure/rest/order/OrderController.kt
+++ src/main/kotlin/io/github/soat7/myburguercontrol/infrastructure/rest/order/OrderController.kt
@@ -7,6 +7,8 @@ import io.github.soat7.myburguercontrol.domain.mapper.toResponse
 import io.github.soat7.myburguercontrol.infrastructure.rest.common.PaginatedResponse
 import io.github.soat7.myburguercontrol.infrastructure.rest.order.api.request.OrderCreationRequest
 import io.github.soat7.myburguercontrol.infrastructure.rest.order.api.response.OrderResponse
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import org.springframework.data.domain.PageRequest
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
@@ -26,22 +28,41 @@ import java.util.UUID
     produces = [MediaType.APPLICATION_JSON_VALUE]
 )
 @CrossOrigin(origins = ["*"], allowedHeaders = ["*"])
+@SecurityRequirement(name = "Bearer Authentication")
 class OrderController(
     private val service: OrderServicePort
 ) {
 
     @PostMapping
+    @Operation(
+        tags = ["2 - Jornada do Pedido"],
+        summary = "Utilize esta rota para criar um pedido",
+        description = "Utilize esta rota para criar um pedido",
+        operationId = "5"
+    )
     fun create(@RequestBody request: OrderCreationRequest): ResponseEntity<OrderResponse> = run {
         val response = service.createOrder(request.toOrderDetails())
         ResponseEntity.ok(response.toResponse())
     }
 
     @GetMapping
+    @Operation(
+        tags = ["2 - Jornada do Pedido"],
+        summary = "Utilize esta rota para encontrar o(s) pedido(s) por cpf de cliente",
+        description = "Utilize esta rota para encontrar o(s) pedido(s) por cpf de cliente",
+        operationId = "6"
+    )
     fun findOrdersByCpf(@RequestParam("cpf") cpf: String): ResponseEntity<List<OrderResponse>> = run {
         ResponseEntity.ok(service.findOrdersByCustomerCpf(cpf).map { it.toResponse() })
     }
 
     @GetMapping("/queue")
+    @Operation(
+        tags = ["2 - Jornada do Pedido"],
+        summary = "Utilize esta rota para encontrar a lista do(s) pedido(s)",
+        description = "Utilize esta rota para encontrar a lista do(s) pedido(s)",
+        operationId = "6"
+    )
     fun findOrderQueue(
         @RequestParam(defaultValue = "0") page: Int,
         @RequestParam(defaultValue = "10") size: Int
